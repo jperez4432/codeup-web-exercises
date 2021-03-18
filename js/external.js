59a9d5b12683e6ea1dc24f8d01f4f0f207f0109b
Juan Perez <jperez4432@gmail.com>
2021-03-18 15:28:35 -0500

    completed the js with html lecture

---
+++ js/external.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+var whatisthisthen = message + "Hope yall are having a good day";
+console.log(whatisthisthen);
+var favoritecheese = prompt(message:"what is your favorite cheese?");
+
+console.log(favoritecheese + "happens to be your favorite cheese");
+
+var typeOfPrompt = typeof prompt(message:"tell me something");
+
+console.log(typeOfPrompt);  */
+
+/*
+var num = parseFloat(prompt("gimme a number..."));
+
+console.log("User entered: " + num + ". That number plus 17 is : " + (num + 17) + ".");
+
+ */
+alert("youre looking at a website");
+
