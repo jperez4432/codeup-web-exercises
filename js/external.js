f694ab2ed8d5a86422c814e662bdc2873fc2dd7a
Juan Perez <jperez4432@gmail.com>
2021-03-19 11:29:59 -0500

    completed the exercise walkthough

--- js/external.js
+++ js/external.js
@@ -1,21 +1,37 @@
 "use strict";
 
-/*
-var whatisthisthen = message + "Hope yall are having a good day";
-console.log(whatisthisthen);
-var favoritecheese = prompt(message:"what is your favorite cheese?");
+console.log("Hello from external Javascript")
 
-console.log(favoritecheese + "happens to be your favorite cheese");
+alert("Welcome to my Website!")
+var favoritecolor = prompt("what is your favorite color?");
+alert("Great! " + favoritecolor + " That is my favorite color too!");
 
-var typeOfPrompt = typeof prompt(message:"tell me something");
+var LM = prompt("How many days did you rent Little Mermaid?")
+var BB = prompt("How many days did you rent Brother Bear?")
+var Hercules = prompt ("How many days did you rent Hercules?")
+var Total = (LM * 3) + (BB * 3 ) + ( Hercules * 3)
+console.log(alert("Great! Your total is " + Total));
 
-console.log(typeOfPrompt);  */
+var Google = prompt("How many hours did you work at Google?")
+var Amazon = prompt("How many hours did you work at Amazon?")
+var Facebook = prompt("How many hours did you work at Facebook?")
+var pay = (Google * 400 )  + ( Amazon * 380 ) + ( Facebook * 350 )
+console.log(alert("Your total pay is $" + pay));
 
-/*
-var num = parseFloat(prompt("gimme a number..."));
+var classSize = confirm("Is the class full?")
+if (classSize == true) {alert("You may not register for this class.")}
+var schedule = confirm("Does this class conflict with your schedule?");
+if (schedule == true) {alert("You may not register for this class")}
+var canEnroll = classSize && schedule
+if (canEnroll == true) {alert("You may not register for this class.")}
+else{alert("Great! You may register for this class!")}
 
-console.log("User entered: " + num + ". That number plus 17 is : " + (num + 17) + ".");
+var expirationDate = confirm("Is the coupon expired?")
+if (expirationDate == false) {alert("This offer is valid!")}
+else  {alert("This offer is invalid")}
 
- */
-alert("youre looking at a website");
+var premium = confirm("Are you a premium member?")
+var itemspurchased = prompt("How many items did you purchase?")
+if ((itemspurchased >= 2 || premium) && expirationDate == false) {alert("The offer is valid!")}
+else {alert("The offer is invalid")}
 
