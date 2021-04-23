206018c71ce9bb0805286f7e254fcbc20267c832
Juan Perez <jperez4432@gmail.com>
2021-04-23 10:38:37 -0500

    dom query exercise

---
+++ js/dc-marvel.js
@@ -0,0 +1,10 @@
+// var accessTheDOM = document.getElementById("dc");
+// var showMe = accessTheDOM.innerHTML;
+// alert(showMe)
+
+var like = document.getElementById("like");
+var dontLike = document.getElementById("dontLike");
+var dc = dc.innerHTML = dc.innerHTML + ' ' + dc.innerHTML;
+var output = dontLike.innerHTML + ' ' + document.getElementById(preference).innerHTML;
+alert(output);
+var marvel
