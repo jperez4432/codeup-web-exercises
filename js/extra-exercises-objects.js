4b31f16cf4dbd3ce1c6b917183080832f1290610
Juan Perez <jperez4432@gmail.com>
2021-04-28 09:02:41 -0500

    extra exercises

---
+++ js/extra-exercises-objects.js
@@ -0,0 +1,25 @@
+var car = {};
+car.make = "VW";
+car.model = "Beetle";
+console.log(car)
+
+
+// var employees = {};
+// employees.employeeNumber = "1"
+// employees.firstName = "Jane";
+// employees.lastName = "Janeway";
+// employees.salesUnit = "3";
+
+var salesReport = [
+    {
+        title: "Monthly Sales Report",
+        date: "03-17-2015",
+        office: "Codeup",
+        employees: {
+            employeeNumber: "1",
+            employeesFirstName: "Jane",
+            employeesLastName: "Janeway",
+            employeesSalesUnit: "3",
+        }
+    }]
+console.log(salesReport);
