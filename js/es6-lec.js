e8749158234b124bddbcd75c2cb4a676693ab89f
Juan Perez <jperez4432@gmail.com>
2021-05-13 17:01:27 -0500

    es6

---
+++ js/es6-lec.js
@@ -0,0 +1,19 @@
+var oldWay = Math.pow(4,2);
+console.log(oldWay);
+
+var newWay = 4** 2;
+console.log(newWay);
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+};
+i = "hello!";
+console.log(i);
+
+if (i === "hello!") {
+    var bubbles = "bububububles";
+}
+console.log(bubbles)
+function test() {
+    var logMe = "Hello!"
+}
