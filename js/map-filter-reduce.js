994849b4043a7da3b2423d1cb843beecb55cfe05
Juan Perez <jperez4432@gmail.com>
2021-05-14 15:57:51 -0500

    troubleshoot why its not displaying in console log

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -67,3 +67,63 @@
 
 
 
+const users = [
+   {
+      id: 1,
+      name: 'ryan',
+      email: 'ryan@codeup.com',
+      languages: ['clojure', 'javascript'],
+      yearsOfExperience: 5
+   },
+   {
+      id: 2,
+      name: 'luis',
+      email: 'luis@codeup.com',
+      languages: ['java', 'scala', 'php'],
+      yearsOfExperience: 6
+   },
+   {
+      id: 3,
+      name: 'zach',
+      email: 'zach@codeup.com',
+      languages: ['javascript', 'bash'],
+      yearsOfExperience: 7
+   },
+   {
+      id: 4,
+      name: 'fernando',
+      email: 'fernando@codeup.com',
+      languages: ['java', 'php', 'sql'],
+      yearsOfExperience: 8
+   },
+   {
+      id: 5,
+      name: 'justin',
+      email: 'justin@codeup.com',
+      languages: ['html', 'css', 'javascript', 'php'],
+      yearsOfExperience: 9
+   }
+];
+
+// es5
+
+let emails =  users.map(function(user){
+   user = user.email;
+   return user;
+});
+
+// ES6
+let es6Emails = users.map(user => user.email);
+
+
+
+// const languages = users.filter(function (){
+//    return users.languages;
+// });
+// console.log(languages)
+//
+//
+// const emails = users.map(function (x){
+//   return users.email
+// });
+// console.log(emails);
