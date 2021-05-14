c1151b169ac37a35e16fde69a1e266ad9c56cd34
Juan Perez <jperez4432@gmail.com>
2021-05-13 22:04:22 -0500

    used object destructuring

--- js/es6.js
+++ js/es6.js
@@ -72,12 +72,17 @@ users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  // const name = user.name;
+  // const email = user.email;
+  // const languages = user.languages;
+  //
+  //
+  const {name, email, languages} = developers;
+  console.log(developers);
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  // developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+
 });
 
 // TODO: Use `let` for the following variable
