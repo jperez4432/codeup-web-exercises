fb3356b5bf58254760c2fc0bd56938537f03a0a4
Juan Perez <jperez4432@gmail.com>
2021-05-17 16:46:40 -0500

    receving api data in console

--- js/promises.js
+++ js/promises.js
@@ -1,1 +1,6 @@
 console.log("Hello");
+
+const promiseExercise =  fetch('https://api.github.com/users', {headers: {'Authorization': GITHUB_ACCESS_TOKEN}});
+promiseExercise.then(response => console.log(response))
+promiseExercise.catch(error => console.log(error));
+
