1100c90f8e9e316f39f31f7d6629641bf9f3d8b6
Juan Perez <jperez4432@gmail.com>
2021-05-17 17:18:53 -0500

    attempting code with teammates assistance

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,28 @@
-console.log("Hello");
+// console.log("Hello");
+//
+// const promiseExercise =  fetch('https://api.github.com/users', {headers: {'Authorization': GITHUB_ACCESS_TOKEN}});
+// promiseExercise.then(response => console.log(response))
+// promiseExercise.catch(error => console.log(error));
 
-const promiseExercise =  fetch('https://api.github.com/users', {headers: {'Authorization': GITHUB_ACCESS_TOKEN}});
-promiseExercise.then(response => console.log(response))
-promiseExercise.catch(error => console.log(error));
+// await octokit.request('GET /users/{username}/received_events', {
+//     username: 'jperez4432'
+// });
 
+// }).then(function(data){
+//     console.log(data)
+//     return data.json()
+// }).then(function(data){
+//     console.log(data)
+// })
+let user = 'jperez4432'
+
+fetch(https://api.github.com/users/${user}/events, {
+method: 'GET',
+    headers: {'Authorization': GITHUB_ACCESS_TOKEN}
+}).then(resp => resp.json())
+
+    .then((resp) => {
+        console.log(resp)
+        let lastPush = resp[0]
+        let createdAt = lastPush.created_at
+        console.log(createdAt));
