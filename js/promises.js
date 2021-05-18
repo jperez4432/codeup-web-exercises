01ef50b90c9593d125b66ce44601cfd5024e71aa
Juan Perez <jperez4432@gmail.com>
2021-05-18 10:02:00 -0500

    completed first exercise

--- js/promises.js
+++ js/promises.js
@@ -1,28 +1,13 @@
-// console.log("Hello");
-//
-// const promiseExercise =  fetch('https://api.github.com/users', {headers: {'Authorization': GITHUB_ACCESS_TOKEN}});
-// promiseExercise.then(response => console.log(response))
-// promiseExercise.catch(error => console.log(error));
+const githubUsername = userName => {
+    return fetch(`https://api.github.com/users/${userName}/events`, {
+        headers: {'Authorization': `token ${GITHUB_ACCESS_TOKEN}`}}).then(response => response.json().then(data => {
+            console.log(data);
+            let latestPush = data[0].created_at
+            let date = new Date(latestPush);
 
-// await octokit.request('GET /users/{username}/received_events', {
-//     username: 'jperez4432'
-// });
+            console.log(date.toString());
+        }))
+    }
+    console.log(githubUsername('jperez4432'));
 
-// }).then(function(data){
-//     console.log(data)
-//     return data.json()
-// }).then(function(data){
-//     console.log(data)
-// })
-let user = 'jperez4432'
 
-fetch(https://api.github.com/users/${user}/events, {
-method: 'GET',
-    headers: {'Authorization': GITHUB_ACCESS_TOKEN}
-}).then(resp => resp.json())
-
-    .then((resp) => {
-        console.log(resp)
-        let lastPush = resp[0]
-        let createdAt = lastPush.created_at
-        console.log(createdAt));
