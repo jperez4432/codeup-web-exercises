9fbae3e295d6b2b046d0ce3cb0f6c60233d4b0cd
Juan Perez <jperez4432@gmail.com>
2021-05-26 09:16:57 -0500

    morning exercise

--- js/warmups.js
+++ js/warmups.js
@@ -1,15 +1,15 @@
-var x = Math.floor((Math.random() * 1000) + 1);
-function fizzbuzz (x) {
-    if (x % 5 === 0 && x % 3 === 0) {
-        console.log("FizzBuzz!");
-    } else if (x % 5 === 0) {
-        console.log("Buzz");
-    } else if (x % 3 === 0) {
-        console.log("Fizz");
-    }
-}
-
-fizzbuzz(x);
+// var x = Math.floor((Math.random() * 1000) + 1);
+// function fizzbuzz (x) {
+//     if (x % 5 === 0 && x % 3 === 0) {
+//         console.log("FizzBuzz!");
+//     } else if (x % 5 === 0) {
+//         console.log("Buzz");
+//     } else if (x % 3 === 0) {
+//         console.log("Fizz");
+//     }
+// }
+//
+// fizzbuzz(x);
 
 // instructor answer
 // (() => {
@@ -28,3 +28,23 @@ fizzbuzz(x);
 //     }
 //     fizzbuzz(30)
 // })();
+// Tuesdays
+// function return7 () {
+//     console.log(7);
+// }
+// return7();
+
+
+//correct answer
+// const multiplyByFive = num => typeof num === "number" ? num * 5 : 0;
+// console.log(multiplyByFive("FJFJF"))
+
+// my attempt
+// function multipliedBy5 (x){
+//     if (x !== isNaN(x)) {
+//     return x * 5
+// } else {
+//         return 0
+//     }
+// }
+// console.log(multipliedBy5(f))
