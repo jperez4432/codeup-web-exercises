0a67323c907b93d20787f3baef9fc63932d9e0c0
Juan Perez <jperez4432@gmail.com>
2021-09-02 14:35:16 -0500

    edited weather map project

---
+++ js/glassdoor.js
@@ -0,0 +1,3 @@
+$.ajax("https://api.glassdoor.com/api/api.htm?t.p=5317&t.k=n07aR34Lk3Y&userip=2600:1700:1882:1da0:9cf3:dc3:8967:90e0&useragent=&format=json&v=1&action=employers").done(function (data) {
+console.log(data);
+});
